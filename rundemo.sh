4b270bfde2ccce77d615fb4b3e73cc495be23da2
EamonZhang <zhang.eamon@hotmail.com>
2024-09-13 09:45:00 +0800

    rebuilding site 2024年 09月 13日 星期五 09:45:00 CST

--- rundemo.sh
+++ rundemo.sh
@@ -1,1 +1,1 @@
-hugo server --baseURL http://demo.zhangeamon.top --bind 10.0.24.11 --disableFastRender --theme=maupassant-hugo --buildFuture
+hugo server  --disableFastRender --theme=maupassant-hugo --buildFuture
